b94b0176903b8d7013fc3edbc022b801224ff50a
sungil <sammychun1@gamil.com>
2019-04-10 14:11:05 +0900

    Initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild BasicBot.sln -p:DeployOnBuild=true -p:PublishProfile=bot-shinnavi-134252-Web-Deploy.pubxml -p:Password=7e2sXw2F87H1zmBRfK3PwoeMryhtkGj1Hxfe00oFGsaqkc9r5xm22CcKJNTY
+
