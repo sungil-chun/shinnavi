1040ad7b79d8319f82ba2337ea8798bfc211e906
jaejunejeun <liopmjuk@gmail.com>
2019-06-17 21:34:04 +0900

    인코딩 변경

--- BasicBot.cs
+++ BasicBot.cs
@@ -164,6 +164,8 @@ namespace Microsoft.BotBuilderSamples
                                                 new HeroCard
                                                 {
                                                     Title = "펀드 매수 화면번호는 1000번입니다.",
+
+
                                                     Buttons = actions,
                                                 }.ToAttachment());
                                             await dc.Context.SendActivityAsync(response);
